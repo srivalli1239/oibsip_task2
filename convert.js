f698a2002c6ab3ed2e1b188e86a82c8c61dd3f4a
srivallisrivalli572 <srivallisrivalli572@gmail.com>
2023-05-15 16:49:24 +0530

    second commit

--- convert.js
+++
@@ -1,7 +0,0 @@
-function celciusToFahrenheit(degree) {
-	displayResult.innerHTML = `${(degree * 1.8 + 32).toFixed(2)}F`;
-}
-
-function FahrenheitToCelcius(degree) {
-	displayResult.innerHTML = `${(((degree - 32) * 5) / 9).toFixed(2)}C`;
-}
